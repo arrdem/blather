e3bebd3e7f9e5c4a27066b7ac8c47ff1bd96922c
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-11-14 23:34:09 -0800

    Working RFC 5234 parser, Instaparse frontend incomplete

--- src/me/arrdem/blather/rfc5234.clj
+++ src/me/arrdem/blather/rfc5234.clj
@@ -1,2 +1,9 @@
 (ns me.arrdem.blather.rfc5234
-  "A Blather dialect for ABNF as specified in RFC5234.")
+  "A Blather dialect for ABNF as specified in RFC5234."
+  (:require [instaparse.core :refer [parser]]            
+            [clojure.java.io :refer [resource]]))
+
+(def -parser
+  (parser (slurp (resource "abnf-rfc5234.insta"))
+          :start :rulelist
+          :auto-whitespace :standard))
