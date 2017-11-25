cbf6caf602489d39a0f860f5c1b6faadc5fe0894
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-11-25 14:48:01 -0800

    A helper

--- src/dev/clj/user.clj
+++ src/dev/clj/user.clj
@@ -1,1 +1,6 @@
 (ns user)
+
+(defn match [pattern string]
+  (let [[string & groups] (re-find pattern string)]
+    {:matched-text string
+     :groups       (into (sorted-map) vector (range 1 Long/MAX_VALUE) groups)}))
