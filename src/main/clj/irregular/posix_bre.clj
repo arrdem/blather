927f502aa7d097ad0da37028ef95ee9ac90ba8aa
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-11-28 22:23:21 -0800

    Evaluating POSIX BREs

--- src/main/clj/irregular/posix_bre.clj
+++ src/main/clj/irregular/posix_bre.clj
@@ -13,7 +13,15 @@
   (parser (slurp (resource "posix-bre.insta"))
           :start :pattern))
 
+(defn as-char-set [char-or-set]
+  (if (instance? Character char-or-set)
+    (s/char char-or-set)
+    char-or-set))
 
+(defn -parse-char-class [& components]
+  (->> components
+       (map as-char-set)
+       (apply s/char-set-union)))
 
 (def -transformer
   (merge
@@ -23,11 +31,16 @@
    m/bounded-repetition
    m/unbounded-repetition
    {;; Wrappers, drop 'em
-    :character       identity
-    :atom            identity
-    :repetition      identity
-    :character-class identity
-    :pattern         identity
+    :repetition             identity
+    :character-class        identity
+    :pattern                identity
+    :character              identity
+    :escaped-character      identity
+    :simple-character-class identity
+
+    ;; The contract is that any Atom must become a char set - so ensure that chars (which don't
+    ;; become sets for convenience elsewhere) get normalized to sets.
+    :atom as-char-set
 
     ;; Parentheticals become groups
     :parenthetical c/group
@@ -36,13 +49,20 @@
     :star-repetition c/rep*
 
     ;; In general we just convert single characters to singleton ranges
-    :simple-character (comp s/char first)
+    :simple-character first
+
+    ;; The special character terminal is used to relieve characters of their semantics, but the
+    ;; standard doesn't specify any escape aliasing. So just pop the wrapper off.
+    :special-character first
 
     ;; . has special semantics and means literally any character
     :any-character (fn [& _] s/ANY-CHAR-RANGE)
 
     ;; Character classes are a little tricky because they have arithmetic & POSIX specific features.
-    :simple-character-class }))
+    :character-range          s/char-range
+    :negative-character-class #(s/char-set-difference s/ANY-CHAR-RANGE %)
+    :positive-character-class -parse-char-class
+    }))
 
 (defn parse
   "Consumes a resource, parsing it as a POSIX BRE, and generating an analyzed FSM"
